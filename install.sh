935d6fa35271ed4f2b92b020bf8152738f9ae81f
David Cain <davidjosephcain@gmail.com>
2013-02-15 13:50:33 -0500

    Simple install script to symlink dotfiles

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# install.sh
+#
+# Installs all the dotfiles (modeled after Michael J. Smalley's script)
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files=".bashrc .bash_aliases .tmux.conf"
+
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+echo "Moving any existing dotfiles from ~ to $olddir"
+for file in $files; do
+    mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
