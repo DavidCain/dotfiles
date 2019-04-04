ac0fa98986de65435fd72a043de500c21466960f
David Cain <davidjosephcain@gmail.com>
2019-04-04 15:43:17 -0700

    Version control dotfiles backups
    Previous iterations of this script had a nasty side effect: Running it
    twice could accidentally remove the original versions of files!
    
    Run 1:
    - Copy existing content to ~/dotfiles_old
    - Replace original dotfiles in ~/
    
    Run 2:
    - Copy dotfiles to ~/dotfiles_old, overwriting saved files
    - Notice that the originals are now gone!
    
    Solve this problem by version-controlling backups with a Git repository.
    This provides a simple, effective way of logging a history.
    
    As a benefit, this also solves the problem of accidentally nesting
    symlinks in vimfiles.

--- install.sh
+++ install.sh
@@ -8,16 +8,48 @@ BACKUP_DIR=$HOME/dotfiles_old/
 FILES=".bashrc .bash_aliases .inputrc .tmux.conf .hushlogin"
 
 mkdir -p $BACKUP_DIR
+pushd $BACKUP_DIR
+git init  # Does nothing if already existing
+git config user.name "dotfiles installer"
+git config user.email "davidjosephcain@gmail.com"
+# If global Git config has a signing key, disable it
+# (We don't care about signing these commits, and don't want to propmpt for passphrase)
+git config commit.gpgsign false
+popd
 
-# First, backup each existing dotfile
+# ---------------------------------------- #
+#               Backup stage               #
+# ---------------------------------------- #
+# First, back up each existing dotfile
 # Then replace with a symlink to version-controlled file
+# We use the pattern of "copy first, then symlink" (instead of `mv`)
+# to always copy _contents_, in case the existing file is a symlink (`mv` preserves symlink)
+
+# Start with normal dotfiles
 for file in $FILES; do
     # Copy file, then overwrite it withe a new symlink
     # (Using `mv` doesn't handle the case where the original was a symlink)
     # We want to backup _contents_, not an inode
-    cp ~/$file $BACKUP_DIR
+    cp ~/$file $BACKUP_DIR/
     ln -fsv $DIR/$file ~/$file
 done
+
+# Finally, handle ~/.vim (which could be a directory, or a symlinked directory)
+if [[ -L $HOME/.vim ]]; then
+    # Existing vimfiles are just a symlink
+    # We can harmlessly remove the directory without causing data loss
+    rm -rf $HOME/.vim
+elif [[ -d $HOME/.vim ]]; then
+    # The existing ~/.vim is a directory. Back it up!
+    rm -rf $BACKUP_DIR/.vim  # (Already committed this revision, if any)
+    mv -v $HOME/.vim $BACKUP_DIR/
+fi
+
+# Commit the changes (if any).
+pushd $BACKUP_DIR
+git add -A .
+git commit -m "Backup dotfiles"
+popd
 
 echo
 echo "Old dotfiles archived in $BACKUP_DIR"
@@ -25,7 +57,8 @@ echo
 
 # Fetch updates to Vim and Git files (or clone repos, if first load)
 cd $DIR
-git submodule update --init --recursive --remote
+echo "Updating all submodules (vimfiles, gitfiles, etc.)"
+git submodule update --init --recursive
 cd -
 
 # Configure Vim
@@ -35,6 +68,7 @@ ln -fsv $HOME/.vim/.vimrc $HOME/.vimrc
 # Configure Git
 ln -fsv $DIR/gitfiles/.gitconfig_global $HOME/.gitconfig
 ln -fsv $DIR/gitfiles/.gitignore_global $HOME/.gitignore
+rm $HOME/.git_template  # Remove existing symlink to avoid nesting
 ln -fsv $DIR/gitfiles/git_template $HOME/.git_template
 
 vim -c "PlugClean | PlugInstall | qa"
